5dd10fd6f3524bfe59090475eeb3e6ac7e3b35d0
Liseth Arias <3397@holbertonschool.com>
2021-06-18 18:17:20 -0700

    1-last_digit.5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 adn not 0\n", n, tlast);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, tlast);
 	}
 	return (0);
 }
