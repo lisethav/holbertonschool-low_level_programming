5c53983b0c89f3feec1a9bf51100acf4ce4cbf2d
Liseth Arias <3397@holbertonschool.com>
2021-06-18 19:52:07 -0700

    7-print_tebahpla.1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Print the abc.. with putchar
+ *
+ * Return: This is the end
+ */
+
+int main(void)
+{
+	int abc = 122;
+
+	while (abc >= 97)
+	{
+		putchar(abc);
+		abc++;
+	}
+	putchar('\n');
+	return (0);
+}
