4288c66ca00373d21b3aa013cd84c56d50547b84
Liseth Arias <3397@holbertonschool.com>
2021-06-18 19:43:46 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Print the numbers
+ *
+ * Return: This is the end
+ */
+
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
