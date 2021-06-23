fff864c5d8e697b838074120890f1711991fd208
Liseth Arias <3397@holbertonschool.com>
2021-06-22 21:04:30 -0700

    punto 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - Print the
+ * @num1: number 1
+ * @num2: number 2
+ * Return: This is the end
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
