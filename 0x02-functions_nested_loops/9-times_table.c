200d840702fcbbab4265133573fb597d4cb98a6d
Liseth Arias <3397@holbertonschool.com>
2021-06-23 17:22:51 -0700

    punto dificil

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * times_table - prints times table 0 to 9
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int a, b, last, first, linend;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			linend = a * b;
+
+			last = ((linend) % 10);
+			first = ((linend - first) / 10);
+			
+			if (first == 0 && b > 0)
+			{
+				_putchar(' ');
+			}
+			else if (b > 0)
+			{
+				_putchar(first + '0');
+			}
+			_putchar(last + '0');
+			if (b == 9)
+			{
+				break;
+			}
+
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
