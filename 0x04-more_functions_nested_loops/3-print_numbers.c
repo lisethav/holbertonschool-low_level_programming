8f58106ea8ac8fd402be50dd945d792e12b350cd
Liseth Arias <3397@holbertonschool.com>
2021-06-24 21:36:51 -0700

    prueba p3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print numbers to 0...9
+ *
+ * Return: the end
+ */
+
+void print_numbers(void)
+{
+       char num;
+
+       for (num = '0'; num <= '9'; num++)
+       {
+	       _putchar(num);
+       }
+       _putchar('\n');
+}
