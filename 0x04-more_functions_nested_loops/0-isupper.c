d5cb1264cf530f9307e96794fbeff9b26148c078
Liseth Arias <3397@holbertonschool.com>
2021-06-24 18:02:21 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _issuper - is C o c
+ *
+ * Return: the end
+ */
+int _issuper(int c)
+{
+	if (c == 'C')
+    {
+        return(1);
+    }
+    else
+    {
+        return(0);
+    }
+}
