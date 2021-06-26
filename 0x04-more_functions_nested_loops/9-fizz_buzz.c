477561eb50cf77187997e219df0781e35d4a9146
Liseth Arias <3397@holbertonschool.com>
2021-06-25 20:40:09 -0700

    punto 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - numbers to 1 - 100 with condicional
+ *
+ * Return: the end
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 15 == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
