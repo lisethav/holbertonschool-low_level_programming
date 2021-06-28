fe0a889ed15fec31d0f867d74544fdfbd4215c60
Liseth Arias <3397@holbertonschool.com>
2021-06-28 15:18:13 -0700

    punto 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 /**
- * void swap_int - print to 98
+ * swap_int - print to 98
  *
  * @a: variable 1
  * @b: variable 2
  * Return: the end.
  */
-
 void swap_int(int *a, int *b)
 {
 	int c = *a;
