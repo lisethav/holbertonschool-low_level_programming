64b9891c0849d26f97975664e1b55892d21a23ad
Liseth Arias <3397@holbertonschool.com>
2021-06-29 12:54:57 -0700

    punto 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/*
+ * _strlen - counts the number of characters.
+ *
+ * @*s: pointer
+ * Return: the counter
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
