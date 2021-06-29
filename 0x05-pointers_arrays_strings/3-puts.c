9257652e9557cc187f31e6efbb961d46c580fec9
Liseth Arias <3397@holbertonschool.com>
2021-06-29 14:36:27 -0700

    punto 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _puts - print to cadena
+ *
+ * @str: pointer
+ * Return: a non-negative integer or EOF.
+ */
+void _puts(char *str)
+{
+	int c;
+
+	c = 0;
+
+	while (c = *str++)
+	{
+		_putchar(c);
+	}
+	return ((c) ? EOF : 0);
+}
