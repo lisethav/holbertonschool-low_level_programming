d08c5ea2eab1e6609bc0dbe6cc96426406d942e5
Liseth Arias <3397@holbertonschool.com>
2021-06-29 21:52:43 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * puts2 - number except impar
+ *
+ * @str: pointer
+ * Return: the end
+ */
+void puts2(char *str)
+{
+	int c;
+
+	c = 0;
+
+	while (str[c] != '\0')
+	{
+		if (c % 2 == 0)
+		{
+			putchar(str[c]);
+		}
+		c++;
+		}
+	putchar('\n');
+}
