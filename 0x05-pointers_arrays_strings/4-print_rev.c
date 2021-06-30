1c7801515a4eaf60c0056f01ee57c02b5c467303
Liseth Arias <3397@holbertonschool.com>
2021-06-29 17:02:52 -0700

    punto 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_rev - print to reverse the text
+ *
+ * @s: pointer
+ * Return: the end.
+ */
+void print_rev(char *s)
+{
+	int j;
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[i--]);
+	}
+	_putchar('\n');
+}
