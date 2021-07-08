3a558d0e9f5243dcf3732523864b5a7fde41283c
Liseth <3397@holbertonscholl.com>
2021-07-07 21:53:31 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - fuction returns the value of x raised to the power of y
+ *
+ * @x: value to expo
+ * @y: exponent variable
+ * Return: If y is lower than 0, the function should return -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
