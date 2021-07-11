de0bb097a4d809a52bc62f94ede07f3a47ec787b
Liseth <3397@holbertonscholl.com>
2021-07-11 16:21:49 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: stands for argument count
+ * @argv: stands for argument values
+ * Return: the end
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%d\n", argc - 1);
+	printf("\n");
+	return (0);
+}
