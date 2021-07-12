bc468c641aecdf503506bb364162d7e8276d0a1d
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-11 21:00:43 -0500

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,9 +7,10 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc < 1)
-		return (-1);
-	printf("%s\n", argv[0]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
 
