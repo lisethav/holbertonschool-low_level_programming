21efeec96308d0b9d03447e24ef8f0cdb52934e2
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-11 19:49:44 -0500

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i])) /* non numeric */
-		{
-			printf("Error\n");
-			return (1);
-		}
 		sum += atoi(argv[i]);
+		printf("%d\n", sum);
+		return (0);
 	}
-	printf("%d\n", sum);
-	return (0);
+	if (!atoi(argv[i]))
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
