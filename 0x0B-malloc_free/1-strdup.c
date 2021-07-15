ee8edd8ae8cfb597383a42da9b05db7ff1917548
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-14 21:20:22 -0500

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,20 +3,21 @@
 /**
  * _strdup - function that returns a pointer to a newly allocated in memory
  *
- * @str: array
+ * @str: string
  * Return: Returns NULL if str = NULL and S
  */
 char *_strdup(char *str)
 {
 	char *s;
-	int i, x;
+	int i = 0, x = 0;
 
 	if (str == 0)
 	{
 		return (NULL);
 	}
 	for (i = 0; str[i] != '\0'; i++)
-		;
+	{
+	}
 	s = malloc(sizeof(char) * i) + 1);
 	if (s == 0)
 	{
