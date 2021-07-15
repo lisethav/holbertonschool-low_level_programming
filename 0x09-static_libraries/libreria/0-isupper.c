be1b863ce2722580db58c3d31f461f01b632b812
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-14 23:10:00 -0500

    Update 0-isupper.c

---
+++ 0x09-static_libraries/libreria/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - is C o c
+ *
+ * @c: its variable
+ * Return: the end
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
