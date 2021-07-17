f03d9bab93baec0ceb29b9fd1374108c3a5238a0
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-17 00:13:15 -0500

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,30 +1,16 @@
 #include <stdlib.h>
-#include <stdio.h>
 /**
   * malloc_checked - function that allocates memory using malloc
   *
   * @b: size
   * Return: pointer
   */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-	char *out;
-	unsigned int len1, len2;
+	void *p;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	if (n >= len2)
-		out = malloc(sizeof(char) * (len1 + len2 + 1));
-	else
-		out = malloc(sizeof(char) * (len1 + n + 1));
-	if (out == NULL)
-		return (NULL);
-
-	out = strcpy(out, s1);
-	out = strncat(out, s2, n);
-	return (out);
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
