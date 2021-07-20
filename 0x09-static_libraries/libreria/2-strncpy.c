9ed8f277837f43534157cffccdad6a8e59939c92
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-19 23:26:05 -0500

    Update 2-strncpy.c

---
+++ 0x09-static_libraries/libreria/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - check the code for Holberton School students.
+ *
+ * @src: string to copy
+ * @dest: destiny
+ * @n: n bytes to copy
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (dest);
+}
