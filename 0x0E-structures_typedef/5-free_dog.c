cca02cda4944b0c2a76cadf758cf4be023cb1167
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-20 22:06:52 -0500

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,19 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * free_dog - frees a dog structure
  *
- * @d: pointer to struct to free
+ * @d: pointer struct to free
  *
- * Return: always void
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
-	if (d != 0)
+	if (d != NULL)
 	{
-		if (d->name != 0)
+		if (d->name != NULL)
 			free(d->name);
-		if (d->owner != 0)
+		if (d->owner != NULL)
 			free(d->owner);
 		free(d);
 	}
