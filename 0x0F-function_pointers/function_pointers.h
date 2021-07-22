8820836e768609ceec7a901d477fa502da0e09f9
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-21 23:03:49 -0500

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef __FUNCTION_POINTERS_H
-#define __FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
