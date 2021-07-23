85125a781da6da68eaf1ce3a0fef3d9919c9772d
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-23 00:41:40 -0500

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -15,16 +15,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}};
 	int i = 0;
-	while (i <= 5)
+	while (i < 5)
 	{
 		if (*(ops[i].op == *s)
-				{
-				return (ops[i].f;
-				}
-				i++;
-	}
-	printf("Error\n");
-	exit(98);
-	
-	return (0);
+		    {
+			    return (ops[i].f);
+		    }
+		    i++;
+		    }
+		    printf("Error\n");
+		    exit(98);
+		    return (0);
 }
