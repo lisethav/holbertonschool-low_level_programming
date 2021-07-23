b7b2787bf5cd13664814b56de78adddb0b5f4e92
Liseth <3397@holbertonscholl.com>
2021-07-22 23:07:46 -0700

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,14 +8,15 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
 	char *operator = argv[2];
 	int a = atoi(argv[1]);
 	int b = atoi(argv[3]);
+
+	if (argc != 4)
+        {
+                printf("Error\n");
+                exit(98);
+        }
 
 	printf("%d\n", get_op_func(operator)(a, b));
 	return (0);
