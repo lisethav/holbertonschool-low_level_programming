d840aa3734d59d20e237762cf87b584735ef057d
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-23 23:35:43 -0500

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,15 +1,16 @@
 #ifndef _CALCULATOR_
 #define _CALCULATOR_
-
+/* LIBRARIES */
 #include <stdlib.h>
 #include <stdio.h>
+/* ESTRUCTURES */
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
 
-/* PROTOTIPO DE FUNCIONES */
+/* FUNCTION PROTOTYPE */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -17,4 +18,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 int (*get_op_func(char *s))(int, int);
-#endif /* Calculator*/
+#endif /* CALCULATOR*/
