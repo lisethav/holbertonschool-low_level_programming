c2a5ba7ddf283fa025dbd524895fa43ed7d69ace
lisethav <85521450+lisethav@users.noreply.github.com>
2021-07-23 21:04:19 -0500

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,1 +1,13 @@
-#include 
+#include "variadic_functions.h"
+/**
+* sum_them_all - sum to all paramete
+*
+* @n: number of parameters
+* Return: int
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+  if (n == 0)
+    return (0)
+    
+}
