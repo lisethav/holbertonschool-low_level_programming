39b482580d5e01c27cf882ca14f622790888a0c4
Liseth <3397@holbertonscholl.com>
2021-07-23 21:47:37 -0700

    betty

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,17 +17,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	
+
 	operator = argv[2];
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	
+
 	if (operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
+
 	printf("%d\n", get_op_func(operator)(a, b));
 	return (0);
 }
