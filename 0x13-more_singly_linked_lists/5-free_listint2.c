ea9be61181a7c3f96ddcca65c7601b38ef988f66
lisethav <lisethav@gmail.com>
2021-08-04 20:25:47 -0500

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_listint2 - frees a listint_t list.
+ *@head: input of type listint_t
+ */
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		*head = (*head)->next;
+		free(head);
+	}
+}
