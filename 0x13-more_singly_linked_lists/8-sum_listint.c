389c3e69ba168362faa277d66068250114cb5223
lisethav <lisethav@gmail.com>
2021-08-04 22:27:58 -0500

    suma

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,9 +2,9 @@
 
 /**
  * sum_listint - returns the sum of all the data (n) of a listint_t linked list
- * @head: pointer to the first node in the list
- *
- * Return: sum of all the data (n) of a listint_t linked list
+ * 
+ * @head: pointer to the first node
+ * Return: if the list is empty, return 0
  */
 int sum_listint(listint_t *head)
 {
